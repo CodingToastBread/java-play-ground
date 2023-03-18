80c1e177fa93ebd67d5f639ae3e8826566969144
CodingToastBread <mickeymouset@naver.com>
2023-03-18 21:46:25 +0900

    more example code for file and directory related java code

--- src/test/java/coding/toast/bread/playing_with_file/JavaFilesApiTest.java
+++ src/test/java/coding/toast/bread/playing_with_file/JavaFilesApiTest.java
@@ -1,37 +1,22 @@
 package coding.toast.bread.playing_with_file;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.test.util.AssertionErrors;
+import org.junit.jupiter.api.*;
 import org.springframework.util.FileSystemUtils;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileAttribute;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
-import java.util.Objects;
 import java.util.Set;
 
+import static java.nio.file.StandardOpenOption.APPEND;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.util.AssertionErrors.assertTrue;
 import static org.springframework.test.util.AssertionErrors.fail;
 
-/**
- * This class is a test class that explores various experiments related to the java.nio.file.Files API.
- * However, for tasks that are difficult to achieve with Files alone, it also utilizes additional utility APIs
- * provided by the Spring framework.
- */
 
 /**
  * <h2>Java Files Api Test</h2>
@@ -47,6 +32,7 @@ import static org.springframework.test.util.AssertionErrors.fail;
  * @see <a href="https://docs.oracle.com/javase/tutorial/essential/io/dirs.html">The Java™ Tutorials</a>
  */
 @Slf4j
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class JavaFilesApiTest {
 	
 	private static final String testFileDirectoryPath
@@ -66,18 +52,59 @@ public class JavaFilesApiTest {
 		// create test folder
 		createNewDirectory(testDirPath);
 		
+		
+		// creating a directory structure for test
+		createSampleDirectory(testDirPath);
+		
 		// simple logging...
 		log.debug("creating %s directory".formatted(testFileDirectoryPath));
 		
 		// if you want to open the folder with right after creation, use the code below.
-		// java.awt.Desktop.getDesktop().open(testDirPath.toFile());
+		java.awt.Desktop.getDesktop().open(testDirPath.toFile());
+	}
+	
+	private static void createSampleDirectory(Path path) throws IOException {
+		
+		// We are going to create a sample directory structure as follows:
+		/*
+		sample_dir
+		|   company_info.txt
+		|
+		+---person
+		|       person_info.txt
+		|       person_info2.txt
+		|
+		\---todo
+		        todo_list.txt
+		 */
+		
+		Path sampleDirPath = path.resolve("sample_dir");
+		Path personPath = sampleDirPath.resolve("person");
+		Path todoPath = sampleDirPath.resolve("todo");
+		
+		// create sub directory inside sample_dir directory
+		createNewDirectory(personPath);
+		createNewDirectory(todoPath);
+		
+		InputStream companyStream = JavaFilesApiTest.class.getResourceAsStream("./company_info.txt");
+		InputStream personStream = JavaFilesApiTest.class.getResourceAsStream("./person_info.txt");
+		InputStream todoStream = JavaFilesApiTest.class.getResourceAsStream("./todo_list.txt");
+		
+		// create sample files
+		Path companyFilePath = sampleDirPath.resolve("company.txt");
+		Path personFilePath = personPath.resolve("person_info.txt");
+		Path todoFilePath = todoPath.resolve("todo_list.txt");
+		
+		Files.copy(companyStream, companyFilePath);
+		Files.copy(personStream, personFilePath);
+		Files.copy(todoStream, todoFilePath);
+		
 	}
 	
 	
 	@Test
 	@DisplayName("file copy test")
 	void copyFileUsingStreamTest() {
-		System.out.println("JavaFilesApiTest.some1");
 		
 		Path personInfoFilePath = testDirPath.resolve("person_info.txt");
 		Path todoListFilePath = testDirPath.resolve("todo_list.txt");
@@ -90,7 +117,6 @@ public class JavaFilesApiTest {
 			fail(e.getMessage());
 			return;
 		}
-		
 		
 		// Method 2: when there was no transferTo Api we had to do like this...
 		try (InputStream is = this.getClass().getResourceAsStream("./todo_list.txt");
@@ -137,14 +163,104 @@ public class JavaFilesApiTest {
 	}
 	
 	
+	// Actually, Spring's FileSystemUtils.deleteRecursively method already provides a way to empty the directory.<br>
+	// However, I still wanted to practice and study by writing the code myself, so I wrote the following test code.
 	@Test
-	void deleteAllFileInsideDirectoryTest() {
+	@Order(2)
+	@DisplayName("delete all files and sub directories")
+	void deleteAllFileInsideDirectoryTest() throws IOException {
 		log.debug("The test code will be written soon.");
+		Path root = testDirPath.resolve("sample_dir");
+		
+		// this method is exactly same with FileSystemUtils.deleteRecursively!!!
+		// i code it, just for practice!
+		Files.walkFileTree(root, new SimpleFileVisitor<>() {
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				Files.delete(file);
+				return FileVisitResult.CONTINUE;
+			}
+			
+			@Override
+			public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+				Files.delete(dir);
+				return FileVisitResult.CONTINUE;
+			}
+		});
 	}
 	
+	
+	// Actually, Spring's FileSystemUtils.copyRecursively method already provides a way to copy the directory.<br>
+	// However, I still wanted to practice and study by writing the code myself, so I wrote the following test code.
 	@Test
-	void creatingNewDirectoryTest() {
-		log.debug("The test code will be written soon.");
+	@Order(1)
+	@DisplayName("copy all files and sub directories")
+	void copyDirectoryTest() throws IOException {
+		
+		// the code below is almost the same with FileSystemUtils.copyRecursively(); method!
+		Path source = testDirPath.resolve("sample_dir");
+		Path destination = testDirPath.resolve("sample_copy_dir");
+		BasicFileAttributes fileAttributes = Files.readAttributes(source, BasicFileAttributes.class);
+		
+		if (fileAttributes.isDirectory()) {
+			Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
+				@Override
+				public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+					Files.createDirectories(destination.resolve(source.relativize(dir)));
+					return FileVisitResult.CONTINUE;
+				}
+				
+				@Override
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+					Files.copy(file, destination.resolve(source.relativize(file)), StandardCopyOption.REPLACE_EXISTING);
+					return FileVisitResult.CONTINUE;
+				}
+			});
+		}
+	}
+	
+	
+	@Test
+	void appendStringAtExistingFile() {
+		
+		// first create sampleFile...
+		Path testFilePath = testDirPath.resolve("appendingString.txt");
+		try {
+			Path file = Files.createFile(testFilePath);
+		} catch (IOException e) {
+			fail("fail creating file in \"appendStringAtExistingFile\" test!");
+		}
+		
+		// Method 1:
+		try {
+			// it will close automatically...
+			Files.writeString(
+				testFilePath,
+				"appending Some Strings...1" + System.lineSeparator(),
+				StandardCharsets.UTF_8, APPEND);
+			
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+		
+		// Method 2:
+		try (BufferedWriter bufferedWriter = Files.newBufferedWriter(testFilePath, StandardCharsets.UTF_8, APPEND);
+		     PrintWriter printWriter = new PrintWriter(bufferedWriter)) {
+			
+			// The bufferedWriter alone is sufficient for adding content to a file,
+			// but if you need to write a sentence and keep adding new lines,
+			// using PrintWriter can be a bit more convenient.
+			printWriter.println("appending Some Strings...2");
+			
+			// The above code performs the same operation as the following code.
+			// bufferedWriter.append("appending Some Strings...2");
+			// bufferedWriter.newLine();
+			
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+		
+		
 	}
 	
 	
