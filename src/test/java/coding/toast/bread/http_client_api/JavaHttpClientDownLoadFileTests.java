96772141f9049077578d903309c80eb756058446
CodingToastBread <mickeymouset@naver.com>
2023-03-24 16:59:41 +0900

    cleaning import statement and add more clear comments in JavaHttpClientDownLoadFileTests.java

--- src/test/java/coding/toast/bread/http_client_api/JavaHttpClientDownLoadFileTests.java
+++ src/test/java/coding/toast/bread/http_client_api/JavaHttpClientDownLoadFileTests.java
@@ -4,10 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.util.AssertionErrors;
-import org.springframework.util.FileCopyUtils;
+import org.springframework.util.FileSystemUtils;
 
-import java.awt.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -15,14 +13,9 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
-import java.util.List;
-import java.util.concurrent.CompletionStage;
-import java.util.concurrent.Flow;
 
 import static org.springframework.test.util.AssertionErrors.fail;
 
@@ -37,23 +30,28 @@ public class JavaHttpClientDownLoadFileTests {
 	static private final Path sampleVideoPath
 		= Path.of(System.getProperty("user.home")).resolve("sample_video");
 	
+	static private final HttpRequest httpRequest = HttpRequest.newBuilder()
+		.GET()
+		.uri(URI.create("https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_2mb.mp4"))
+		.build();
+	
+	/**
+	 * creating Test Directory before test begins<br>
+	 * @apiNote : if you want to automatically open directory as soon as it is created,
+	 * uncomment the last line of this method.
+	 */
 	@BeforeAll
 	static void beforeAll() throws IOException {
+		FileSystemUtils.deleteRecursively(sampleVideoPath);
+		Files.createDirectories(sampleVideoPath);
 		// Desktop.getDesktop().open(sampleVideoPath.toFile());
 	}
 	
 	@Test
 	void downloadFileTest() throws IOException, InterruptedException {
-		HttpRequest httpRequest = HttpRequest.newBuilder()
-			.GET()
-			.uri(URI.create("https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_2mb.mp4"))
-			.build();
 		
 		HttpClient httpClient = HttpClient.newHttpClient();
 		
-		// if the path doesn't exist, create one.
-		Files.createDirectories(sampleVideoPath);
-
 		HttpResponse<Path> video1 = httpClient.send(httpRequest,
 			HttpResponse.BodyHandlers.ofFile(sampleVideoPath.resolve("1.mp4")));
 		
@@ -70,11 +68,6 @@ public class JavaHttpClientDownLoadFileTests {
 	@DisplayName("Showing Loading Process While Downloading")
 	void showLoadingBarTest() {
 		
-		HttpRequest httpRequest = HttpRequest.newBuilder()
-			.GET()
-			.uri(URI.create("https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_2mb.mp4"))
-			.build();
-		
 		HttpClient httpClient = HttpClient.newHttpClient();
 		
 		try {
@@ -89,7 +82,7 @@ public class JavaHttpClientDownLoadFileTests {
 			try (InputStream is = videoStream.body();
 			     OutputStream os = Files.newOutputStream(sampleVideoPath.resolve("loading.mp4"), StandardOpenOption.CREATE)
 			) {
-				byte[] buffer = new byte[8192 * 2];
+				byte[] buffer = new byte[8192 * 4];
 				int bufferSize = buffer.length;
 				int readByteLength;
 				int readTotalByte = 0;
